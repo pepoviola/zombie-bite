0b3ccf3bb790c36431594d598c8371930ca38da5
Javier Viola <javier@parity.io>
2024-09-18 22:59:44 +0200

    fmt

--- src/doppelganger.rs
+++ src/doppelganger.rs
@@ -5,9 +5,9 @@ use std::str::FromStr;
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
 
+use flate2::write::GzEncoder;
 use flate2::Compression;
 use tar::Builder;
-use flate2::write::GzEncoder;
 
 use zombienet_configuration::NetworkConfigBuilder;
 use zombienet_orchestrator::network::Network;
@@ -16,8 +16,8 @@ use zombienet_provider::types::RunCommandOptions;
 use zombienet_provider::types::SpawnNodeOptions;
 use zombienet_provider::DynNamespace;
 use zombienet_provider::DynProvider;
-use zombienet_provider::Provider;
 use zombienet_provider::NativeProvider;
+use zombienet_provider::Provider;
 use zombienet_support::fs::local::LocalFileSystem;
 
 #[tokio::main]
@@ -30,34 +30,38 @@ async fn main() {
     let filesystem = LocalFileSystem;
     let provider = NativeProvider::new(filesystem.clone());
     let epoch_ms = get_epoch_ms();
-    let fixed_base_dir = PathBuf::from_str(format!("/tmp/zombie-bite_{epoch_ms}").as_str()).unwrap();
+    let fixed_base_dir =
+        PathBuf::from_str(format!("/tmp/zombie-bite_{epoch_ms}").as_str()).unwrap();
     let base_dir_str = fixed_base_dir.to_string_lossy();
     let ns = provider
         .create_namespace_with_base_dir(fixed_base_dir.as_path())
         .await
         .unwrap();
 
-
-    let _stdout = run_doppelganger_node(ns.clone(), &fixed_base_dir).await.unwrap();
+    let _stdout = run_doppelganger_node(ns.clone(), &fixed_base_dir)
+        .await
+        .unwrap();
 
     // get the chain-spec (prod) and clean the bootnodes
     let chain_spec_path = format!("{}/spec.json", &base_dir_str);
-    generate_chain_spec(ns.clone(), &chain_spec_path).await.unwrap();
+    generate_chain_spec(ns.clone(), &chain_spec_path)
+        .await
+        .unwrap();
 
     // generate the data.tgz to use as snapshot
     let snap_path = format!("{}/snap.tgz", &base_dir_str);
     generate_snap(&base_dir_str, &snap_path).await.unwrap();
 
     // config a new network with alice/bob
-	let config = NetworkConfigBuilder::new()
-		.with_relaychain(|r| {
-			r.with_chain("kusama")
-				.with_default_command("polkadot")
+    let config = NetworkConfigBuilder::new()
+        .with_relaychain(|r| {
+            r.with_chain("kusama")
+                .with_default_command("polkadot")
                 .with_chain_spec_path(PathBuf::from(&chain_spec_path))
                 .with_default_db_snapshot(PathBuf::from(&snap_path))
-				.with_node(|node| node.with_name("alice"))
-				.with_node(|node| node.with_name("bob"))
-		})
+                .with_node(|node| node.with_name("alice"))
+                .with_node(|node| node.with_name("bob"))
+        })
         .build()
         .unwrap();
 
@@ -72,17 +76,21 @@ async fn main() {
     loop {}
 }
 
-async fn spawn(provider: DynProvider, chain_spec_path: &str, snap_path: &str) -> Result<Network<LocalFileSystem>, String> {
+async fn spawn(
+    provider: DynProvider,
+    chain_spec_path: &str,
+    snap_path: &str,
+) -> Result<Network<LocalFileSystem>, String> {
     // config a new network with alice/bob
-	let config = NetworkConfigBuilder::new()
-		.with_relaychain(|r| {
-			r.with_chain("kusama")
-				.with_default_command("polkadot")
+    let config = NetworkConfigBuilder::new()
+        .with_relaychain(|r| {
+            r.with_chain("kusama")
+                .with_default_command("polkadot")
                 .with_chain_spec_path(PathBuf::from(&chain_spec_path))
                 .with_default_db_snapshot(PathBuf::from(&snap_path))
-				.with_node(|node| node.with_name("alice"))
-				.with_node(|node| node.with_name("bob"))
-		})
+                .with_node(|node| node.with_name("alice"))
+                .with_node(|node| node.with_name("bob"))
+        })
         .build()
         .unwrap();
 
@@ -105,25 +113,21 @@ async fn generate_snap(base_dir: &str, snap_path: &str) -> Result<(), String> {
 
     println!("✅ generated with path {snap_path}");
     Ok(())
-
 }
 
 async fn generate_chain_spec(ns: DynNamespace, chain_spec_path: &str) -> Result<(), String> {
     println!("\n📝 Generating chain-spec without bootnodes...");
 
-
-    let temp_node =
-    ns.spawn_node(
-        &SpawnNodeOptions::new("temp-polkadot", "bash")
-            .args(vec![
-                "-c", "while :; do sleep 60; done"
-                ]),
-    )
-    .await.unwrap();
+    let temp_node = ns
+        .spawn_node(
+            &SpawnNodeOptions::new("temp-polkadot", "bash")
+                .args(vec!["-c", "while :; do sleep 60; done"]),
+        )
+        .await
+        .unwrap();
 
     let cmd_stdout = temp_node
-        .run_command(RunCommandOptions::new("polkadot")
-        .args(vec![
+        .run_command(RunCommandOptions::new("polkadot").args(vec![
             "build-spec",
             "--chain",
             "kusama",
@@ -147,29 +151,37 @@ async fn generate_chain_spec(ns: DynNamespace, chain_spec_path: &str) -> Result<
 async fn run_doppelganger_node(ns: DynNamespace, base_path: &Path) -> Result<(), String> {
     let data_path = format!("{}/sync_db", &base_path.to_string_lossy());
     let logs_path = format!("{}/sync.log", &base_path.to_string_lossy());
-    println!("⛓  Syncing using warp, this could take a while. You can follow the logs with: \n\t
-    tail -f {}", &logs_path);
+    println!(
+        "⛓  Syncing using warp, this could take a while. You can follow the logs with: \n\t
+    tail -f {}",
+        &logs_path
+    );
 
-    let temp_node =
-    ns.spawn_node(
-        &SpawnNodeOptions::new("temp-doppelganger", "bash")
-            .args(vec![
-                "-c", "while :; do sleep 60; done"
-                ]),
-    )
-    .await.unwrap();
+    let temp_node = ns
+        .spawn_node(
+            &SpawnNodeOptions::new("temp-doppelganger", "bash")
+                .args(vec!["-c", "while :; do sleep 60; done"]),
+        )
+        .await
+        .unwrap();
 
     let _stdout = temp_node
-        .run_command(RunCommandOptions::new("bash")
-        .args(vec![
-            "-c", format!("doppelganger --chain kusama --sync warp -d {} > {} 2>&1", &data_path, &logs_path).as_str(),
-        ]))
+        .run_command(
+            RunCommandOptions::new("bash").args(vec![
+                "-c",
+                format!(
+                    "doppelganger --chain kusama --sync warp -d {} > {} 2>&1",
+                    &data_path, &logs_path
+                )
+                .as_str(),
+            ]),
+        )
         .await
         .unwrap();
 
     temp_node.destroy().await.unwrap();
 
-     println!("✅ Synced");
+    println!("✅ Synced");
 
     Ok(())
 }
@@ -181,7 +193,6 @@ fn get_epoch_ms() -> u128 {
         .as_millis()
 }
 
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -189,7 +200,9 @@ mod test {
     #[tokio::test(flavor = "multi_thread")]
     async fn test_snap() {
         let snap_path = "/tmp/zombie-bite_1726677980197/snap.tgz";
-        let demo = generate_snap("/tmp/zombie-bite_1726677980197", &snap_path).await.unwrap();
+        let demo = generate_snap("/tmp/zombie-bite_1726677980197", &snap_path)
+            .await
+            .unwrap();
         println!("{:?}", demo);
         // let _n = spawn(provider, chain_spec_path, snap_path).await.unwrap();
     }
@@ -202,5 +215,4 @@ mod test {
         let snap_path = "/tmp/zombie-bite_1726677980197/snap.tgz";
         let _n = spawn(provider, chain_spec_path, snap_path).await.unwrap();
     }
-
 }
--- src/utils.rs
+++ src/utils.rs
@@ -3,9 +3,8 @@ use std::path::Path;
 use anyhow::anyhow;
 use serde::de::DeserializeOwned;
 use tokio::fs::File;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt,AsyncWriteExt};
-
 
 pub async fn get_random_port() -> u16 {
     let listener = TcpListener::bind("0.0.0.0:0".to_string())
@@ -18,35 +17,40 @@ pub async fn get_random_port() -> u16 {
         .port()
 }
 
-
 /// Read the file's content into a [`Vec<u8>`].
 async fn read_file_to_vec<P>(path: P) -> Result<Vec<u8>, anyhow::Error>
 where
-	P: AsRef<Path>,
+    P: AsRef<Path>,
 {
-	let mut file = tokio::fs::File::open(&path).await.map_err(|_| anyhow!("Error opening file {}", path.as_ref().to_string_lossy()))?;
-	let mut content = Vec::new();
+    let mut file = tokio::fs::File::open(&path)
+        .await
+        .map_err(|_| anyhow!("Error opening file {}", path.as_ref().to_string_lossy()))?;
+    let mut content = Vec::new();
 
-	file.read_to_end(&mut content).await.map_err(|_| anyhow!("Error reading file {}", path.as_ref().to_string_lossy()))?;
+    file.read_to_end(&mut content)
+        .await
+        .map_err(|_| anyhow!("Error reading file {}", path.as_ref().to_string_lossy()))?;
 
-	Ok(content)
+    Ok(content)
 }
-
 
 /// Read the file's content into a struct implemented [`DeserializeOwned`].
 pub async fn read_file_to_struct<P, T>(path: P) -> Result<T, anyhow::Error>
 where
-	P: AsRef<Path>,
-	T: DeserializeOwned,
+    P: AsRef<Path>,
+    T: DeserializeOwned,
 {
+    let content = read_file_to_vec(&path).await?;
 
-	let content = read_file_to_vec(&path).await?;
+    let result = serde_json::from_slice(&content).map_err(|_| {
+        anyhow!(
+            "Error deserializing  file {}",
+            path.as_ref().to_string_lossy()
+        )
+    })?;
 
-	let result = serde_json::from_slice(&content).map_err(|_| anyhow!("Error deserializing  file {}", path.as_ref().to_string_lossy()))?;
-
-	Ok(result)
+    Ok(result)
 }
-
 
 /// Write the data to file at the given path.
 ///
@@ -54,9 +58,14 @@ where
 /// Otherwise, this will create a file at the given path.
 pub async fn write_data_to_file<P>(path: P, data: &[u8]) -> Result<(), anyhow::Error>
 where
-	P: AsRef<Path>,
+    P: AsRef<Path>,
 {
-	let mut file = File::create(&path).await.map_err(|_| anyhow!("Error creating file {}", path.as_ref().to_string_lossy()))?;
+    let mut file = File::create(&path)
+        .await
+        .map_err(|_| anyhow!("Error creating file {}", path.as_ref().to_string_lossy()))?;
 
-	Ok(file.write_all(data).await.map_err(|_| anyhow!("Error writting file {}", path.as_ref().to_string_lossy()))?)
+    Ok(file
+        .write_all(data)
+        .await
+        .map_err(|_| anyhow!("Error writting file {}", path.as_ref().to_string_lossy()))?)
 }
